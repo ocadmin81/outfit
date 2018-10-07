6680f35f6f1b05b1488c315eabe0a5e74b654084
Eran Greenwald <eran@originalconcepts.co.il>
2018-10-07 18:10:48 +0300

    Theme creation + languages

---
+++ wp-content/themes/outfit-theme/template-parts/header/site-branding.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Displays header site branding
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-branding">
+	<div class="wrap">
+		<div class="right header-right">
+			<div class="publish"><a href="#"><?php _e( "What's this?", 'loco-translate' ); ?></a></div>
+			<div class="favourites"></div>
+			<div class="login"></div>
+		</div>
+		<?php the_custom_logo(); ?>
+		<div class="left header-left"></div>
+
+		<?php if ( ( twentyseventeen_is_frontpage() || ( is_home() && is_front_page() ) ) && ! has_nav_menu( 'top' ) ) : ?>
+		<a href="#content" class="menu-scroll-down"><?php echo twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ); ?><span class="screen-reader-text"><?php _e( 'Scroll down to content', 'twentyseventeen' ); ?></span></a>
+	<?php endif; ?>
+
+	</div><!-- .wrap -->
+</div><!-- .site-branding -->
