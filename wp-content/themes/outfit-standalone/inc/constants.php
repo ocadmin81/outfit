f084f6c76e456e53eaf1a5f0ac68ab1b54f9568e
Milla Shub <milla@originalconcepts.co.il>
2018-10-17 09:11:16 +0300

    Perfit constants

---
+++ wp-content/themes/outfit-standalone/inc/constants.php
@@ -0,0 +1,12 @@
+<?php
+
+define('USER_META_PHONE', 'phone');
+define('USER_META_FIRSTNAME', 'first_name');
+define('USER_META_LASTNAME', 'last_name');
+define('USER_META_PREFERRED_HOURS', 'outfit_preferred_hours');
+define('USER_META_PRIMARY_ADDRESS', 'outfit_primary_address');
+define('USER_META_SECONDARY_ADDRESSES', 'outfit_secondary_addresses');
+define('USER_META_KIDS_BIRTHDAYS', 'outfit_kids_birthdays');
+define('USER_META_FAVORITE_BRANDS', 'outfit_favorite_brands');
+define('USER_META_ABOUT', 'outfit_about');
+//define('USER_META_', '');
