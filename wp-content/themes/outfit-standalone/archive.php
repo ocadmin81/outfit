6a34eb59f42bcf7a2afbd8c7aaeaf0e4600c5ccd
Eran Greenwald <eran@originalconcepts.co.il>
2018-10-24 16:04:32 +0300

    Blog Page

--- wp-content/themes/outfit-standalone/archive.php
+++ wp-content/themes/outfit-standalone/archive.php
@@ -11,19 +11,17 @@
  */
 
 get_header(); ?>
-
+<div class="top-blog">
+	<?php if ( have_posts() ) : ?>
+		<?php
+			the_archive_title( '<h1 class="page-title">', '</h1>' );
+			the_archive_description( '<div class="taxonomy-description">', '</div>' );
+		?>
+	<?php endif; ?>
+</div>
 <div class="wrap">
 
-	<?php if ( have_posts() ) : ?>
-		<header class="page-header">
-			<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="taxonomy-description">', '</div>' );
-			?>
-		</header><!-- .page-header -->
-	<?php endif; ?>
-
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area full">
 		<main id="main" class="site-main" role="main">
 
 		<?php
@@ -55,7 +53,7 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-	<?php get_sidebar(); ?>
+	<?php //get_sidebar(); ?>
 </div><!-- .wrap -->
 
 <?php get_footer();
